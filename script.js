c26093e0e2d868f3672104e5a32b042288500e43
Austin <ausbratton@gmail.com>
2025-08-22 13:50:52 -0500

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,18 @@
+function loadGrid(numberOfSquares) {
+    const grid = document.getElementById("grid");
+
+    for (let i = 0; i < numberOfSquares; i++) {
+        const square = document.createElement("div");
+        square.style.boxSizing = "border-box";
+        square.style.backgroundColor = "white";
+        square.style.border = "0.5px solid rgb(0, 0, 0, 0.15)";
+        square.style.width = "32px";
+        square.style.height = "32px";
+        grid.appendChild(square);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const defaultNumberOfSquares = 16 * 16;
+    loadGrid(defaultNumberOfSquares);
+});
