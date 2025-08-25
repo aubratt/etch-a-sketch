773679b17eb626f9e74ecccd199a54f75d473a63
Austin <ausbratton@gmail.com>
2025-08-25 11:51:25 -0500

    Change color of square count buttons when clicked

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-function loadGrid(numberOfSquares) {
+function loadGrid(squareCount) {
     const grid = document.getElementById("grid");
 
-    for (let i = 0; i < numberOfSquares; i++) {
+    for (let i = 0; i < squareCount; i++) {
         const square = document.createElement("div");
         square.className = "square";
         square.style.boxSizing = "border-box";
         square.style.backgroundColor = "white";
-        square.style.border = "0.5px solid rgb(0, 0, 0, 0.15)";
-        square.style.width = "32px";
-        square.style.height = "32px";
+        square.style.border = "1px solid rgb(0, 0, 0, 0.15)";
+        square.style.width = "2rem";
+        square.style.height = "2rem";
         grid.appendChild(square);
     }
 }
 
+function changeSquareCount(clickedSquaresBtn) {
+    changeSelectedButton(clickedSquaresBtn);
+}
+
+function changeSelectedButton(clickedSquaresBtn) {
+    clickedSquaresBtn.style.backgroundColor = "lime";
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    const defaultNumberOfSquares = 16 * 16;
-    loadGrid(defaultNumberOfSquares);
+    const defaultSquareCount = 16 * 16;
+    loadGrid(defaultSquareCount);
 
     document.querySelectorAll(".square").forEach((square) => {
-        square.addEventListener("mouseover", function() {
+        square.addEventListener("mouseover", function () {
             square.style.backgroundColor = "black";
+        });
+    });
+
+    document.querySelectorAll(".squares-btn").forEach((btn) => {
+        btn.addEventListener("click", function(event) {
+            changeSquareCount(event.target);
         });
     });
 });
