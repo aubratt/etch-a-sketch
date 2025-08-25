fba0e61534d4396afef16fd687e56c674c8e3ebd
Austin <ausbratton@gmail.com>
2025-08-25 13:30:08 -0500

    Add full functionality to square count buttons

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function loadGrid(squareCount) {
+function loadGrid(squareCount, squareSize) {
     const grid = document.getElementById("grid");
+
+    const currentSquares = document.querySelectorAll(".square");
+    if (currentSquares) {
+        currentSquares.forEach((square) => {
+            square.remove();
+        });
+    }
 
     for (let i = 0; i < squareCount; i++) {
         const square = document.createElement("div");
@@ -7,33 +14,48 @@ function loadGrid(squareCount) {
         square.style.boxSizing = "border-box";
         square.style.backgroundColor = "white";
         square.style.border = "1px solid rgb(0, 0, 0, 0.15)";
-        square.style.width = "2rem";
-        square.style.height = "2rem";
+        square.style.width = squareSize;
+        square.style.height = squareSize;
         grid.appendChild(square);
+        square.addEventListener("mouseover", function() {
+            square.style.backgroundColor = "black";
+        })
     }
 }
 
-function changeSquareCount(clickedSquaresBtn) {
-    changeSelectedButton(clickedSquaresBtn);
-}
-
 function changeSelectedButton(clickedSquaresBtn) {
-    clickedSquaresBtn.style.backgroundColor = "lime";
+    const previousSelected = document.querySelector(".selected");
+    previousSelected.classList.remove("selected");
+
+    clickedSquaresBtn.classList.add("selected");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     const defaultSquareCount = 16 * 16;
-    loadGrid(defaultSquareCount);
-
-    document.querySelectorAll(".square").forEach((square) => {
-        square.addEventListener("mouseover", function () {
-            square.style.backgroundColor = "black";
-        });
-    });
+    const defaultSquareSize = "2rem";
+    loadGrid(defaultSquareCount, defaultSquareSize);
 
     document.querySelectorAll(".squares-btn").forEach((btn) => {
-        btn.addEventListener("click", function(event) {
-            changeSquareCount(event.target);
+        btn.addEventListener("click", function (event) {
+            let squareCount;
+            let squareSize;
+
+            if (event.target.id === "eight") {
+                squareCount = 8 * 8;
+                squareSize = "4rem";
+            } else if (event.target.id === "sixteen") {
+                squareCount = 16 * 16;
+                squareSize = "2rem";
+            } else if (event.target.id === "thirty-two") {
+                squareCount = 32 * 32;
+                squareSize = "1rem";
+            } else {
+                squareCount = 64 * 64;
+                squareSize = "0.5rem";
+            }
+
+            loadGrid(squareCount, squareSize);
+            changeSelectedButton(event.target);
         });
     });
 });
